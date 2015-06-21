78c4759403930d671ee06d5db84f78d6f4878eb1
Marko <mivanovic@outlook.com>
2015-06-21 17:35:21 +0200

    Gruntfile.js and  package.json added, added names for urls.py

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    imagemin: {                          // Task
+      dynamic: {                         // Another target
+        options: {                       // Target options
+          optimizationLevel: 3,
+          svgoPlugins: [{ removeViewBox: false }]
+        },
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'img/',                   // Src matches are relative to this path
+          src: ['*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'img/'                  // Destination path prefix
+        }]
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'static/css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'static/css/',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'js/output.min.js': [
+                                'static/js/jquery-migrate-1.2.1.js',
+                                'static/js/jquery.ui.totop.js'
+                                ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['imagemin', 'cssmin', 'uglify']);
+
+};
--- finalst/urls.py
+++ finalst/urls.py
@@ -6,11 +6,11 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-                       url(r'^$', IndexView.as_view()),
-                       url(r'^references/(?P<id>.+?)/$', SingleReferenceView.as_view()),
-                       url(r'^references/$', ReferencesView.as_view()),
-                       url(r'^contact/send/$', SendMail.as_view()),
-                       url(r'^contact/$', ContactView.as_view()),
+                       url(r'^$', IndexView.as_view(), name='index'),
+                       url(r'^references/(?P<id>.+?)/$', SingleReferenceView.as_view(), name='reference_id'),
+                       url(r'^references/$', ReferencesView.as_view(), name='reference'),
+                       url(r'^contact/send/$', SendMail.as_view(), name='send_email'),
+                       url(r'^contact/$', ContactView.as_view(), name='contact'),
 
                        url(r'^admin/', include(admin.site.urls)),
                        )
